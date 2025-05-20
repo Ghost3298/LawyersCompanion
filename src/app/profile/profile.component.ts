a03d3158fddb9a69fed9d740827836708021216f
Ghost3298 <eddy.lebbos@hotmail.com>
2025-05-20 21:05:12 +0300

    starting profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
 
+  public deleteAccountDiv: boolean = false;
+  public editName : boolean = false;
+  public editPass : boolean = false;
+
+  toggleDeleteAccount(){
+    this.deleteAccountDiv = !this.deleteAccountDiv;
+  }
+
+  toggleEditName(){
+    this.editName = !this.editName;
+  }
+
+  toggleEditPass(){
+    this.editPass = !this.editPass;
+  }
 }
